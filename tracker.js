2584518b2428becc34bc45e12af8cdf03f945d62
iamabutalha <maabutalhakhalil@gmail.com>
2024-08-29 09:50:50 +0500

    Added settimeout to error-message

---
+++ tracker.js
@@ -0,0 +1,90 @@
+let expanses = JSON.parse(localStorage.getItem("expanses")) || [];
+
+function renderExpanses() {
+  let expanseHTML = ``;
+  expanses.forEach((expanse) => {
+    // If the type of expanse is expanse it will be - and + will be for income
+    let sign = expanse.type === "income" ? "+" : "-";
+    // if the type is income the card will be green if it is expans it will be black bcz we are adding classes to it
+    let type = expanse.type === "income" ? "income" : "expanse";
+    expanseHTML += `<div class="transaction ${type}">
+          <span>${expanse.name}</span>
+          <span>${sign} $${expanse.amount.toFixed(1)}</span>
+          <button class="delete-btn">Delete</button>
+        </div>`;
+  });
+
+  document.querySelector(".transactions-container").innerHTML = expanseHTML;
+  totalAmount();
+
+  document.querySelectorAll(".delete-btn").forEach((deleteBtn, index) => {
+    deleteBtn.addEventListener("click", () => {
+      expanses.splice(index, 1);
+      saveToStorage();
+      renderExpanses();
+      totalAmount();
+    });
+  });
+}
+
+document.querySelector("#expense-form").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // Take name from the input field
+  let name = document.querySelector("#description").value;
+  console.log(name);
+  // Take amount from the amount field and stiree it in a variable
+  let amount = parseFloat(document.querySelector("#amount").value);
+  console.log(amount);
+
+  // Take type from the select field
+  let type = document.querySelector("#type").value;
+  console.log(type);
+
+  if (isNaN(amount)) {
+    document.querySelector(".error-message").innerHTML = "Enter a valid amount";
+    setTimeout(() => {
+      document.querySelector(".error-message").innerHTML = "";
+    }, 3000);
+
+    return;
+  }
+
+  expanses.unshift({
+    name: name,
+    amount: amount,
+    type: type,
+  });
+  saveToStorage();
+  renderExpanses();
+  resetValues();
+});
+
+renderExpanses();
+totalAmount();
+
+console.log(expanses);
+
+function saveToStorage() {
+  localStorage.setItem("expanses", JSON.stringify(expanses));
+}
+
+function totalAmount() {
+  let totalAmount = 0.0;
+  expanses.forEach((expanse) => {
+    if (expanse.type === "income") {
+      totalAmount += expanse.amount;
+    } else {
+      totalAmount -= expanse.amount;
+    }
+  });
+
+  document.querySelector("#total-balance").innerHTML = ` $${totalAmount.toFixed(
+    1
+  )}`;
+}
+
+function resetValues() {
+  let form = document.querySelector("#expense-form");
+  form.reset();
+}
